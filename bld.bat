6cc9b0eb3e5a1f1a09405d41a50f0c6820bfb7ae
DerKevinRiehl <50080024+DerKevinRiehl@users.noreply.github.com>
2021-01-24 00:42:47 +0100

    Add files via upload

---
+++ bld.bat
@@ -0,0 +1,10 @@
+set -eu -o pipefail
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+mkdir -p $PREFIX/bin
+copy -R * $outdir/
+copy $RECIPE_DIR/javatest2.py $outdir/javatest2
+ls -l $outdir
+ln -s $outdir/javatest2 $PREFIX/bin
+chmod 0755 "${PREFIX}/bin/javatest2"
---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu -o pipefail
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir
+mkdir -p $PREFIX/bin
+cp -R * $outdir/
+cp $RECIPE_DIR/javatest2.py $outdir/javatest2
+ls -l $outdir
+ln -s $outdir/javatest2 $PREFIX/bin
+chmod 0755 "${PREFIX}/bin/javatest2"
